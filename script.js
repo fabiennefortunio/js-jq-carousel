4cfc2bc2c5c6c19ffd0c361440006d9e67d1b597
Fabienne <fortunio.fabienne@gmail.com>
2019-11-27 18:03:50 +0100

    prev done

--- script.js
+++ script.js
@@ -1,22 +1,36 @@
 $(document).ready(function() {
     //call to action clicking the right icon
-    $( '.icons' ).click(function() {
+
+    $( '.right' ).click(function() {
 
 
         var img_corrente = $('img.active');
         var img_successiva = img_corrente.next('img');
-        if(img_successiva.length ==0) {
+        if(img_successiva.length == 0) {
             // se non ci sono più immagini->recupero la prima
             img_successiva = $('.first')
 
-
         }
-//ora diventa display none
-        img_corrente.removeClass('active');
+        //ora diventa display none
+        $(img_corrente).removeClass('active');
         //aggiungo la classe active al img succesiva
-        img_successiva.addClass('active');
+        $(img_successiva).addClass('active');
 
-    })
+    });
 
+        $('.left').click(function() {
+            var img_corrente = $('img.active');
+            var img_precedente = img_corrente.prev('img');
+            if(img_precedente.length == 0) {
+                // se non ci sono più immagini->recupero la prima
+                $(img_precedente) = $('.last'); 
+
+            }
+
+            $(img_corrente).removeClass('active');
+            //aggiungo la classe active al img succesiva
+            $(img_precedente).addClass('active');
+
+        })
 
 });
