569d2d8feec9e360a754afb89594d266226c4b6f
Fabienne <fortunio.fabienne@gmail.com>
2019-11-27 17:17:43 +0100

    script section

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 $(document).ready(function() {
-    $( '.right' ).click(function() {
+    //call to action clicking the right icon
+    $( '.icons' ).click(function() {
 
-      $( "container" ).next().( "img" );
 
-    });
+        var img_corrente = $('img.active');
+        var img_successiva = img_corrente.next('img');
+//ora diventa display none
+        img_corrente.removeClass('active');
+        //aggiungo la classe active al img succesiva
+        img_successiva.addClass('active');
 
-    $( ".left" ).click(function() {
-      alert( 'prova' );
-});
+    })
+
+
 });
