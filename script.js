f8af617e07ff07a5c84c266f1e0ff2c9523381bd
Fabienne <fortunio.fabienne@gmail.com>
2019-11-27 17:24:26 +0100

    next fatto

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ $(document).ready(function() {
 
         var img_corrente = $('img.active');
         var img_successiva = img_corrente.next('img');
+        if(img_successiva.length ==0) {
+            // se non ci sono più immagini->recupero la prima
+            img_successiva = $('.first')
+
+
+        }
 //ora diventa display none
         img_corrente.removeClass('active');
         //aggiungo la classe active al img succesiva
