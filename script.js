16586c830b99c5cd138ad0def4c0c425b537edd8
Fabienne <fortunio.fabienne@gmail.com>
2019-11-27 16:55:31 +0100

    small changes

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
-    
+    $( '.right' ).click(function() {
+
+      $( "container" ).next().( "img" );
+
+    });
+
+    $( ".left" ).click(function() {
+      alert( 'prova' );
+});
 });
