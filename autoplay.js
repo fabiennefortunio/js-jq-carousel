b1386962822cc5db41f63926d2193f01cee041db
Fabienne <fortunio.fabienne@gmail.com>
2019-11-28 13:57:08 +0100

    new file autoplay.js added

---
+++ autoplay.js
@@ -0,0 +1,28 @@
+// AUTOPLAY
+
+var start = ('left', 'right') // user fa click sulla freccia destra o sinistra
+//creo una funzione per l'Autoplay (user fa click)
+function autoplay(start) {
+    autoplay = true;
+    setInterval(function(autoplay), 2000);
+// creo una funzione per richiamarla quando l'utente fa click di nuovo o una basta?
+//autoPlay.function(stop)
+
+
+
+
+    $( '.left' .'right' ).click(function() {
+    //user fa click
+    //comincia il ciclo quando l'utente fa click
+    // cominciano a scorrere le immagine
+    //richiama la funzioen e scorri le immagini ogni 2 secondi
+    do {
+        function(autoPlay)
+    } while {
+        //user non fa click di nuovo
+    } if {
+        //l'utente clicca sulla immagina ferma il ciclo ---> ClearInterval
+    }
+    }
+
+}
