33b807cadf05a08b21cea41374021fb694a00e44
Fabienne <fortunio.fabienne@gmail.com>
2019-11-29 10:35:55 +0100

    autoplay

--- script.js
+++ script.js
@@ -35,28 +35,9 @@ alert('hola');
 
         });
 
-});
-// GIRO DELLE IMMAGINE DEL CARROUSEL
 
-/*var autoplay = true;
-if();
-
-    do {
-        setInterval(function(autoplay, 3000);
-    } while {
-        $( '.right' '.left' ).click(function() {
-            if (autoplay = false) {
-                clearInterval(autoplay);
-            }
-
-        }
-
-    }
-}
-
-};
-*/
-
+// aggiungere l'autoplay allo slider
+/*
 autoplay = function () {
     var img_corrente = $('img.active');
     var img_successiva = img_corrente.next('img');
@@ -69,35 +50,33 @@ autoplay = function () {
     $(img_corrente).removeClass('active');
     //aggiungo la classe active al img succesiva
     $(img_successiva).addClass('active');
-}
+}*/
 
 
- //dati quando l'utente fa click una volta
- var start = ('.right' '.left');
- console.log('start');
- //dati quando l'utente fa click di nuovo
+ //dati quando l'utente fa click
+    /* var start = $('.freccia');
+     console.log(start);
 
- //funzione con click
+
+
      //se l'utente fa click le immagini cominciano a girare
-     if  $( 'start' ).click(function() {
+    start.click(function() {
          setInterval(function(autoplay(), 3000);
-}
+     })
+*/
 
-    // se l'utente clicca di nuovo la operazione si ferma
-    if  $( 'start' ).click(function() {
-        clearInterval(autoplay());
-}
+//funzione
+        setInterval(function () {
+        var img_corrente = $('img.active');
+        var img_successiva = img_corrente.next('img');
+        if(img_successiva.length == 0) {
+            // se non ci sono più immagini->recupero la prima
+            img_successiva = $('.first')
 
-setInterval(function () {
-    var img_corrente = $('img.active');
-    var img_successiva = img_corrente.next('img');
-    if(img_successiva.length == 0) {
-        // se non ci sono più immagini->recupero la prima
-        img_successiva = $('.first')
-
-    }
-    //ora diventa display none
-    $(img_corrente).removeClass('active');
-    //aggiungo la classe active al img succesiva
-    $(img_successiva).addClass('active');
-},3000)
+        }
+        //ora diventa display none
+        $(img_corrente).removeClass('active');
+        //aggiungo la classe active al img succesiva
+        $(img_successiva).addClass('active');
+    },3000);
+});
